0c84e842a8ba0188245feada845cc9e1cdd8424d
anhbaysgalan1 <anhbaysgalan1@gmail.com>
2025-01-04 23:23:12 +0800

    update search service

--- search.go
+++ search.go
@@ -23,12 +23,19 @@ type SearchParams struct {
 	PerPage  *int    `json:"per_page,omitempty"`
 }
 
+type SearchResponse struct {
+	Found        int         `json:"found"`
+	OutOf        int         `json:"out_of"`
+	Page         int         `json:"page"`
+	SearchTimeMs int         `json:"search_time_ms"`
+	Hits         []SearchHit `json:"hits"`
+}
+
 type Document interface {
 	GetCreatedAt() time.Time
 	GetMetaData() map[string]interface{}
 }
 
-// Use existing types as documents
 func (l *Ledger) GetCreatedAt() time.Time             { return l.CreatedAt }
 func (l *Ledger) GetMetaData() map[string]interface{} { return l.MetaData }
 
@@ -40,15 +47,6 @@ func (t *Transaction) GetMetaData() map[string]interface{} { return t.MetaData }
 
 type SearchHit struct {
 	Document Document `json:"document"`
-}
-
-type SearchResponse struct {
-	Found         int          `json:"found"`
-	OutOf         int          `json:"out_of"`
-	Page          int          `json:"page"`
-	RequestParams SearchParams `json:"request_params"`
-	SearchTimeMs  int          `json:"search_time_ms"`
-	Hits          []SearchHit  `json:"hits"`
 }
 
 func (s *SearchService) SearchDocument(params SearchParams, resource ResourceType) (*SearchResponse, *http.Response, error) {
